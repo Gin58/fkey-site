d6edc73e947b61bb3ccd277657381923b5868a53
Gin58 <33783892+Gin58@users.noreply.github.com>
2023-01-01 23:04:40 +0900

    add babelrc

--- src/components/molecules/HamburgerIcon.tsx
+++ src/components/molecules/HamburgerIcon.tsx
@@ -1,9 +1,10 @@
 import cntl from "cntl"
+import styled from "styled-components"
 
 export const HamburgerIcon = () => {
   return (
     <>
-      <span className={FirstBorder} />
+      <FirstSpan />
       <span className={SecondBorder} />
       <span className={ThirdBorder} />
     </>
@@ -18,6 +19,13 @@ const Border = cntl`
   rounded
 `
 
+const FirstSpan = styled.span`
+  position: absolute;
+  background-color: #222222;
+  width: 100%;
+  height: 0.125rem;
+  top: 0;
+`
 const FirstBorder = Border + ' top-0'
 const SecondBorder = Border + ' top-2.5'
 const ThirdBorder = Border + ' bottom-0'
