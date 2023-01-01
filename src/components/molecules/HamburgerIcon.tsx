9a80aeb45eceee6e84413414854ee4e0c1fe5905
Gin58 <33783892+Gin58@users.noreply.github.com>
2023-01-01 23:25:30 +0900

    add keyframes

--- src/components/molecules/HamburgerIcon.tsx
+++ src/components/molecules/HamburgerIcon.tsx
@@ -1,15 +1,70 @@
 import cntl from "cntl"
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
 
 export const HamburgerIcon = () => {
   return (
     <>
-      <FirstSpan />
-      <span className={SecondBorder} />
-      <span className={ThirdBorder} />
+      <Span />
+      <Span />
+      <Span />
     </>
   )
 }
+
+const closeMenuBarFirst = keyframes`
+  0% {
+    transform: translateY(10px) rotate(45deg);
+  }
+
+  50% {
+    transform: translateY(10px) rotate(0);
+  }
+
+  100% {
+    transform: translateY(0) rotate(0);
+  }
+`
+
+const closeMenuBarLast = keyframes`
+  0% {
+    transform: translateY(-10px) rotate(-45deg);
+  }
+
+  50% {
+    transform: translateY(-10px) rotate(0);
+  }
+
+  100% {
+    transform: translateY(0) rotate(0);
+  }
+`
+
+const Span = styled.span`
+  position: absolute;
+  left: 0;
+  display: inline-block;
+  width: 100%;
+  height: 2px;
+  background-color: #222222;
+  border-radius: 4px;
+  transition: all 0.4s;
+
+  &:nth-of-type(1) {
+    top: 0;
+    animation: ${closeMenuBarFirst} 0.75s;    
+  }
+
+  &:nth-of-type(2) {
+    top: 10px;
+    opacity: 1;
+    transition: all 0.25s 0.25s;
+  }
+
+  &:nth-of-type(3) {
+    bottom: 0;
+    animation: ${closeMenuBarLast} 0.75s; 
+  }
+`
 
 const Border = cntl`
   absolute
