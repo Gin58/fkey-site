fa842d0c7a6f34fa5d2e070eddcc69a996aa52aa
Gin58 <33783892+Gin58@users.noreply.github.com>
2023-01-02 17:11:05 +0900

    add HamburgerList

--- src/components/organisms/Hamburger.tsx
+++ src/components/organisms/Hamburger.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import { HamburgerIcon } from '@/components/molecules/HamburgerIcon'
+import { HamburgerList } from '@/components/molecules/HamburgerList'
 export const Hamburger = () => {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <div className="relative w-8 h-[22px] cursor-pointer" onClick={() => setIsOpen((bool) => !bool)}>
-      <HamburgerIcon isOpen={isOpen} />
-    </div>
-
+    <>
+      <div className="fixed top-4 right-4 w-8 h-[22px] cursor-pointer z-50" onClick={() => setIsOpen((bool) => !bool)}>
+        <HamburgerIcon isOpen={isOpen} />
+      </div>
+      <HamburgerList isOpen={isOpen} />
+    </>
   )
 }
