d0830ca82528f64c615c2fb6dd669aa6d5f1a950
Gin58 <33783892+Gin58@users.noreply.github.com>
2023-01-09 16:14:17 +0900

    fix text

--- src/components/molecules/TopHeroText.tsx
+++ src/components/molecules/TopHeroText.tsx
@@ -3,7 +3,7 @@ import cntl from "cntl"
 export const TopHeroText = () => {
   return (
     <div className={Text}>
-      <h1 className="text-3xl md:text-5xl mb-4 font-bold">Web Specialist Company</h1>
+      <h1 className="text-3xl md:text-5xl mb-4 font-bold tracking-wider">Web Specialist Company</h1>
       <span className="text-lg md:text-2xl">株式会社サイバーシップスは、ホームページ制作、システム開発、webマーケティングの専門会社です。</span>
     </div>
   )
