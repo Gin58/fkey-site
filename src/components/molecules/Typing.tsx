c3260eec0618ec3856db1914078bd9553c50d798
Gin58 <33783892+Gin58@users.noreply.github.com>
2023-01-22 22:52:55 +0900

    fix typing

--- src/components/molecules/Typing.tsx
+++ src/components/molecules/Typing.tsx
@@ -1,18 +1,20 @@
-import { memo, useEffect, useState } from 'react'
+import { memo } from 'react'
 import { useTyping } from '../../hooks/useTyping'
 
 type Props = {
   cursor?: boolean
   className?: string
   speed?: number
+  delay?: number
 }
 
 export const Typing = memo(function Typing({
   cursor = true,
   className = '',
-  speed = 50
+  speed = 50,
+  delay = 0.5
 }: Props) {
-  const { text } = useTyping('Web Specialist Company', speed)
+  const { text } = useTyping('Web Specialist Company', speed, delay)
 
   return (
     <div
--- src/hooks/useTyping.tsx
+++ src/hooks/useTyping.tsx
@@ -1,22 +1,25 @@
 import { useEffect, useState } from "react"
+const sleep = (second: number) => new Promise(resolve => setTimeout(resolve, second * 1000))
 
-export const useTyping = (message: string, speed = 50) => {
+export const useTyping = (message: string, speed = 50, delay = 0) => {
   const [text, setText] = useState('')
-
   useEffect(() => {
-    const charItr = message[Symbol.iterator]()
-    let timerId: number
-    const showChar = () => {
-      const nextChar = charItr.next()
-      if (nextChar.done) {
-        return
+    ;(async () => {
+      await sleep(delay)
+      const charItr = message[Symbol.iterator]()
+      let timerId: number
+      const showChar = () => {
+        const nextChar = charItr.next()
+        if (nextChar.done) {
+          return
+        }
+        setText(current => current + nextChar.value)
+        timerId = setTimeout(showChar, speed)
       }
-      setText(current => current + nextChar.value)
-      timerId = setTimeout(showChar, speed)
-    }
-    showChar()
-
-    return () => clearTimeout(timerId);
+      showChar()
+  
+      return () => clearTimeout(timerId);
+    })()
   }, [])
 
   return {
